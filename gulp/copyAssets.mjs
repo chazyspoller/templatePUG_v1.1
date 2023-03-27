1c0f561ccfdaadf0b554989f64c1ce745d8ce3f9
chazyspoller <63188230+chazyspoller@users.noreply.github.com>
2023-03-27 18:10:39 +0300

    fix npm packages (optimaze images)

--- gulp/copyAssets.mjs
+++ gulp/copyAssets.mjs
@@ -1,4 +1,6 @@
 import gulp from 'gulp';
+import ttf2woff from 'gulp-ttf2woff';
+import ttf2woff2 from 'gulp-ttf2woff2';
 
 const copySvg = () => gulp.src('source/img/**/*.svg', {base: 'source'}).pipe(gulp.dest('build'));
 
@@ -6,9 +8,23 @@ const copyImages = () => gulp.src('source/img/**/*.{png,jpg,jpeg,webp}', {base: 
 
 const copy = () =>
   gulp
-      .src(['source/**.html', 'source/fonts/**', 'source/img/**', 'source/favicon/**', 'source/data/**'], {
-        base: 'source',
-      })
-      .pipe(gulp.dest('build'));
+    .src(['source/**.html', 'source/fonts/**', 'source/img/**', 'source/favicon/**', 'source/data/**'], {
+      base: 'source',
+    })
+    .pipe(gulp.dest('build'));
 
-export {copy, copyImages, copySvg};
+const fonts = () =>
+  gulp
+    .src(['source/fonts/*.ttf'], {
+      base: 'source',
+    })
+    .pipe(ttf2woff())
+    .pipe(gulp.dest('build'))
+    .src(['source/fonts/*.ttf'], {
+      base: 'source',
+    })
+    .pipe(ttf2woff2())
+    .pipe(gulp.dest('build'))
+
+
+export {copy, copyImages, copySvg, fonts};
--- gulp/optimizeImages.mjs
+++ gulp/optimizeImages.mjs
@@ -3,57 +3,45 @@ import rename from 'gulp-rename';
 import imagemin from 'gulp-imagemin';
 import webp from 'gulp-webp';
 import svgstore from 'gulp-svgstore';
-import pngQuant from 'imagemin-pngquant';
-import mozJpeg from 'imagemin-mozjpeg';
 import svgo from 'imagemin-svgo';
 
 const sprite = () =>
   gulp
-      .src('source/img/sprite/*.svg')
-      .pipe(svgstore({inlineSvg: true}))
-      .pipe(rename('sprite.svg'))
-      .pipe(gulp.dest('build/img'));
+    .src('source/img/sprite/*.svg')
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
 
 const optimizeSvg = () =>
   gulp
-      .src('build/img/**/*.svg')
-      .pipe(
-          imagemin([
-            svgo({
-              plugins: [
-                {
-                  name: 'removeViewBox',
-                  active: false,
-                },
-                {
-                  name: 'removeRasterImages',
-                  active: true,
-                },
-                {
-                  name: 'removeUselessStrokeAndFill',
-                  active: false,
-                }],
-            })]))
-      .pipe(gulp.dest('build/img'));
+    .src('build/img/**/*.svg')
+    .pipe(
+      imagemin([
+        svgo({
+          plugins: [
+            {
+              name: 'removeViewBox',
+              active: false,
+            },
+            {
+              name: 'removeRasterImages',
+              active: true,
+            },
+            {
+              name: 'removeUselessStrokeAndFill',
+              active: false,
+            }],
+        })]))
+    .pipe(gulp.dest('build/img'));
 
-const optimizeJpg = () =>
-  gulp
-      .src('build/img/**/*.{jpg,jpeg}')
-      .pipe(imagemin([mozJpeg({quality: 90, progressive: true})]))
-      .pipe(gulp.dest('build/img'));
-
-const optimizePng = () =>
-  gulp
-      .src('build/img/**/*.png')
-      .pipe(
-          imagemin([
-            pngQuant({
-              speed: 1,
-              strip: true,
-              dithering: 1,
-              quality: [0.8, 0.9],
-            })]))
-      .pipe(gulp.dest('build/img'));
+const optimizeImages = () => {
+  return gulp.src('build/img/**/*.{png,jpg}')
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+    ]))
+    .pipe(gulp.dest('build/img'));
+};
 
 /*
   Optional tasks
@@ -69,9 +57,9 @@ const optimizePng = () =>
 const createWebp = () => {
   const root = '';
   return gulp
-      .src(`source/img/${root}**/*.{png,jpg}`)
-      .pipe(webp({quality: 90}))
-      .pipe(gulp.dest(`source/img/${root}`));
+    .src(`source/img/${root}**/*.{png,jpg}`)
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest(`source/img/${root}`));
 };
 
-export {sprite, createWebp, optimizeSvg, optimizePng, optimizeJpg};
+export {sprite, createWebp, optimizeSvg, optimizeImages};
